0c335ff3de833a4a1371317fac7de217fed90b27
Apelsin16 <apelsin@i.ua>
2020-04-03 16:42:26 +0300

    start

---
+++ src/components/app/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {
+    text: ''
+  }
+
+  handleChange () {
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+
+        Learn React
+
+      </header>
+      </div>
+    );
+  }
+
+}
+
+export default App;
